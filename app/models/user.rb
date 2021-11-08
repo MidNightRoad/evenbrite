202a716ae4b2b1c97f6308a57a2e1eedb32769fc
MidNightRoad <bergerpierre09@gmail.com>
2021-11-08 18:43:37 +0100

    lalala

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
 has_many :attendances
 has_many :evenement, through: :attendances
 has_many :evenements, foreign_key: 'evenement_id', class_name: 'Evenement'
+after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
