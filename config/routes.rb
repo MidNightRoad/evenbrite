140059f644744a5949cde24d1e770a7bbce00647
MidNightRoad <bergerpierre09@gmail.com>
2021-11-09 15:21:47 +0100

    last

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'static_pages/secret'
   resources :events
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   resources :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
